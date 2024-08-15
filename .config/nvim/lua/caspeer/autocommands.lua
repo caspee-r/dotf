b1916c801ef311a2b4c5bd65f2ed46585e390ce2
caspeer <mr.ash_x7@protonmail.ch>
2024-08-15 14:10:09 +0100

    new location

---
+++ .config/nvim/lua/caspeer/autocommands.lua
@@ -0,0 +1,45 @@
+-- All my Autocmd
+local autocmd = vim.api.nvim_create_autocmd
+--local Caspeer = vim.api.nvim_create_augroup
+
+vim.api.nvim_create_augroup("colorcolumn", { clear = true })
+vim.api.nvim_create_augroup("helpfiles",{clear = true})
+
+vim.cmd(" au TextYankPost * silent! lua vim.highlight.on_yank()")
+
+vim.cmd [[
+autocmd FileType help nnoremap <buffer> <CR> <C-]>
+autocmd FileType help nnoremap <buffer> <BS> <C-T>
+autocmd FileType help nnoremap <buffer> o /'\l\{2,\}'<CR>
+autocmd FileType help nnoremap <buffer> O ?'\l\{2,\}'<CR>
+autocmd FileType help nnoremap <buffer> s /\|\zs\S\+\ze\|<CR>
+autocmd FileType help nnoremap <buffer> S ?\|\zs\S\+\ze\|<CR>
+]]
+
+vim.cmd [[
+augroup locallist
+    autocmd!
+    " Populate locallist with lsp diagnostics automatically 
+    autocmd User LspDiagnosticsChanged :lua vim.lsp.diagnostic.set_loclist({open_loclist = false})
+augroup END
+]]
+
+
+-- Delete [No Name] buffers
+autocmd("BufHidden", {
+  desc = "Delete [No Name] buffers",
+  callback = function(event)
+    if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+      vim.schedule(function() pcall(vim.api.nvim_buf_delete, event.buf, {}) end)
+    end
+  end,
+})
+
+autocmd("TermOpen", {
+  desc = "drop in to the term in insert mode",
+  callback = function(event)
+	  vim.bo.modifiable = true
+	  vim.api.nvim_input("i")
+  end,
+})
+vim.cmd[[autocmd TermOpen * setlocal nonumber norelativenumber]]
---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,191 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"saadparwaiz1/cmp_luasnip",
+	},
+	config = function()
+		local kind_icons = {
+			Text = "󰊄",
+			Method = "m",
+			Function = "󰊕",
+			Constructor = "",
+			Field = "",
+			Variable = "",
+			Class = "",
+			Interface = "",
+			Module = "",
+			Property = "",
+			Unit = "",
+			Value = "",
+			Enum = "",
+			Keyword = "",
+			Snippet = "",
+			Color = "",
+			File = "󰈔",
+			Reference = "",
+			Folder = "",
+			EnumMember = "",
+			Constant = "󰏿",
+			Struct = "",
+			Event = "",
+			Operator = "",
+			TypeParameter = "",
+		}
+
+		local cmp = require "cmp"
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities())
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"clangd",
+				"lua_ls",
+				"bashls",
+				"rust_analyzer",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup {
+						capabilities = capabilities
+					}
+				end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup {
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = { version = "Lua 5.1" },
+								diagnostics = {
+									globals = { "vim", "it", "describe", "before_each", "after_each" },
+								}
+							}
+						}
+					}
+				end,
+				["rust_analyzer"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.rust_analyzer.setup {
+						capabilities = capabilities,
+						autostart = false,
+						settings = {
+							diagnostics = {
+								enable = true,
+							},
+							single_file_support = true,
+						}
+					}
+				end,
+				["clangd"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.clangd.setup {
+						capabilities = capabilities,
+						autostart = false,
+						settings = {
+							single_file_support = true,
+						}
+					}
+				end,
+			}
+		})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				--["<C-d>"] = cmp.open_docs(),
+				["<Return>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+
+			view = {
+				docs = {
+					auto_open = false,
+				}
+			},
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s ", kind_icons[vim_item.kind])
+					-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+					return vim_item
+				end,
+				vim.cmd,
+			},
+
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' }, -- For luasnip users.
+				{ name = 'path' },
+			}, {
+				{ name = 'buffer' },
+			}),
+
+			confirm_opts = {
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = false,
+			},
+			experimental = {
+				ghost_text = true,
+				native_menu = false,
+			},
+		})
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "caspeer",
+				prefix = "",
+			},
+		})
+
+		cmp.setup.cmdline({ '/', '?' }, {
+			mapping = {
+				["<C-j>"] = { c = cmp.mapping.select_next_item() },
+				["<C-k>"] = { c = cmp.mapping.select_prev_item() },
+			},
+			sources = {
+				{ name = 'buffer' }
+			}
+		})
+
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(':', {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = 'path' }
+			}, {
+				{
+					name = 'cmdline',
+					options = {
+						ignore_cmds = { 'Man', '!' }
+					}
+				}
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false },
+		})
+	end
+
+}
